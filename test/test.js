6f12a9105fdc191e4ae8d91e26205b10c8d04013
Mathieu Lecarme <mathieu@garambrogne.net>
2011-12-10 00:20:18 +0100

    More filters.

--- test/test.js
+++ test/test.js
@@ -4,10 +4,14 @@ var vows = require('vows'),
     filter = require('../lib/elasticsearch.js').filter;
 
 var conversion = {
+    '(foo=bar)': {"term": {"foo": "bar"}},
+    //'foo~=bar'
+    //'(!foo=bar)': {},
     '(&(foo=bar)(zig=zag))':
         {"bool":{"must":[{"term":{"zig":"zag"}},{"term":{"foo":"bar"}}]}},
     '(&(|(givenname=pl*)(sn=pl*)(mail=pl*)(cn=pl*)))':
         {"bool":{"must":[{"bool":{"should":[{"prefix":{"cn":"pl"}},{"prefix":{"mail":"pl"}},{"prefix":{"sn":"pl"}},{"prefix":{"givenname":"pl"}}]}}]}}
+
 };
 
 var v = vows.describe('Ldap');
